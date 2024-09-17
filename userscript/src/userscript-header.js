d71febcb971d49ae9fbd3b2fc04d55c5047a7064
zhaobo <vkbo@qq.com>
2024-09-17 22:56:27 +0800

    feat: some comments

--- userscript/src/userscript-header.js
+++ userscript/src/userscript-header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name     React Userscripts
-// @namespace https://github.com/siefkenj/react-userscripts
-// @version  1.1
+// @name     Userscripts for Igloo Products
+// @namespace https://github.com/vkboo/tampermonkey-userscripts-igloo
+// @version  1.0
 // @description A sample userscript built using react
 // @include https://*.google.com*
 // @grant    none
