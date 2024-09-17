7357befca835f33e447e7438587d17843123637f
zhaobo <vkbo@qq.com>
2024-09-17 15:47:59 +0800

    first commit

---
+++ userscript/src/userscript-header.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name     React Userscripts
+// @namespace https://github.com/siefkenj/react-userscripts
+// @version  1.1
+// @description A sample userscript built using react
+// @include https://*.google.com*
+// @grant    none
+// ==/UserScript==
+
