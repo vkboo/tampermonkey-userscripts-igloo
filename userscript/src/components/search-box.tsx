a986fc357685992561d2a0451a534fda74e49439
zhaobo <vkbo@qq.com>
2024-09-29 23:30:25 +0800

    feat: hover & focus style

--- userscript/src/components/search-box.tsx
+++ userscript/src/components/search-box.tsx
@@ -22,19 +22,19 @@ const SearchBox: FC<Props> = ({
     return (
         /**
          * TODO
-         * 1. 初始需要透明
-         * 2. focus/hover input的样式问题
+         * 1. 初始需要透明 ✅
+         * 2. focus/hover input的样式问题 ✅
          * 3. 快捷键的使用
          * 4. hostKey部分，在已经有搜索值的时候，改成"Esc"
          */
         // opacity-65
         <div className={cn(
-            'inline-flex items-center justify-center gap-1 w-72 bg-slate-800 hover:bg-slate-700 px-3 rounded-md',
+            'group inline-flex items-center justify-center gap-1 w-72 bg-slate-800 hover:bg-slate-700 px-3 rounded-md transition-all opacity-65 focus-within:opacity-100',
             className
         )}>
             <MagnifyingGlassIcon className="text-slate-400" width={24} height={24} />
             <Input
-                className="focus:opacity-100 transition-all border-none px-0 rounded-none hover:bg-inherit flex-1"
+                className="border-none px-0 rounded-none group-hover:bg-slate-700 flex-1"
                 placeholder='Quick search...'
             />
             <span className="text-slate-500 text-sm">
